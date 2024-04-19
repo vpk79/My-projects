0eb29521861c0e5ecef954b96699a4862fbe1f4e
vpk79 <someone_bul@yahoo.com>
2024-04-19 18:17:13 +0300

    up

--- Responsive_Book_Store/assets/js/main.js
+++ Responsive_Book_Store/assets/js/main.js
@@ -1,5 +1,23 @@
 /*=============== SEARCH ===============*/
 
+const searchButton = document.getElementById('search-button'),
+    searchClose = document.getElementById('search-close'),
+    searchContent = document.getElementById('search-content')
+
+/*=============== SHOW MENU ===============*/
+/* Menu show */
+if (searchButton) {
+    searchButton.addEventListener('click', () => {
+        searchContent.classList.add('show-search')
+    })
+}
+
+/* Menu hidden */
+if (searchClose) {
+    searchClose.addEventListener('click', () => {
+        searchContent.classList.remove('show-search')
+    })
+}
 
 /*=============== LOGIN ===============*/
 
