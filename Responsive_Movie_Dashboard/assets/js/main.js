81feced75a34e12c8594efaef9f6af8a26950b6b
vpk79 <someone_bul@yahoo.com>
2024-04-22 11:32:57 +0300

    up

--- Responsive_Movie_Dashboard/assets/js/main.js
+++ Responsive_Movie_Dashboard/assets/js/main.js
@@ -1,5 +1,21 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav'),
+    headerMenu = document.getElementById('header-menu'),
+    navClose = document.getElementById('nav-close')
 
+/* Menu show */
+if (headerMenu) {
+    headerMenu.addEventListener('click', () => {
+        nav.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        nav.classList.remove('show-menu')
+    })
+}
 
 /*=============== SWIPER MOVIE ===============*/
 
