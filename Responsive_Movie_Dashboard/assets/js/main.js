32b13f1d9e4d0af3864acdd2535e83f24a25de4b
vpk79 <someone_bul@yahoo.com>
2024-04-22 14:37:32 +0300

    up

--- Responsive_Movie_Dashboard/assets/js/main.js
+++ Responsive_Movie_Dashboard/assets/js/main.js
@@ -68,4 +68,10 @@ let swiperNew = new Swiper('.new__swiper', {
 });
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
